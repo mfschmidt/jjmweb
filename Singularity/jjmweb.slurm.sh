4c8b638eb6aa0258a20870823cb04bdbd59e8dbc
Mike Schmidt <mikeschmidt@schmidtgracen.com>
2018-04-23 12:09:40 -0400

    store logs in subfolder (subfolder must be manually created prior to running)

--- Singularity/jjmweb.slurm.sh
+++ Singularity/jjmweb.slurm.sh
@@ -5,7 +5,7 @@
 #SBATCH -n 1                 # We only need one core
 #SBATCH --mem 1024M          # Allocate a gigabyte, probably overkill
 #SBATCH -t 0                 # No time limit should be imposed
-#SBATCH -o jjmweb.%N.%j.out  # STDOUT
-#SBATCH -e jjmweb.%N.%j.err  # STDERR
+#SBATCH -o $HOME/slurmlogs/jjmweb.%N.%j.out  # STDOUT
+#SBATCH -e $HOME/slurmlogs/jjmweb.%N.%j.err  # STDERR
 
 singularity run --bind /opt/jjmdb /home/webrunner/jjmweb.simg
