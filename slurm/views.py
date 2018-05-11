5e76e08c126361ffe14a362bf7895eedf367c7fd
Mike Schmidt <mikeschmidt@schmidtgracen.com>
2018-05-11 07:45:47 -0400

    Added a configuration div

--- slurm/views.py
+++ slurm/views.py
@@ -12,31 +12,58 @@ def index(request):
 
     # Issue the command to the underlying shell, capturing stdout
     try:
-        slurm_date = subprocess.run(['date'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+        slurm_date = subprocess.run(
+            ['date'], stdout=subprocess.PIPE
+        ).stdout.decode('utf-8')
     except FileNotFoundError:
         slurm_date = "not even a date function"
     try:
-        slurm_queue = subprocess.run(['squeue'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+        slurm_queue = subprocess.run(
+            ['squeue'], stdout=subprocess.PIPE
+        ).stdout.decode('utf-8')
     except FileNotFoundError:
         slurm_queue = ' no squeue found '
     try:
-        slurm_info = subprocess.run(['sinfo'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+        slurm_info = subprocess.run(
+            ['sinfo'], stdout=subprocess.PIPE
+        ).stdout.decode('utf-8')
     except FileNotFoundError:
         slurm_info = ' no sinfo found '
     try:
-        slurm_version = subprocess.run(['squeue', '--version'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+        slurm_version = subprocess.run(
+            ['squeue', '--version'], stdout=subprocess.PIPE
+        ).stdout.decode('utf-8')
     except FileNotFoundError:
         slurm_version = 'SLURM does not appear to be installed.'
-
+    try:
+        slurm_config = subprocess.run(
+            ['cat', '/etc/slurm-llnl/slurm.conf', '|', 'grep', '#'], stdout=subprocess.PIPE
+        ).stdout.decode('utf-8')
+    except FileNotFoundError:
+        slurm_config = 'cannot get config file'
+    
     # Formulate the html to return (This should be made a template, eventually)
-    response = "<h2>squeue</h2>\n"
-    response += "<div style=\"background-color: black; color: white; \"><p>\n<pre>{}</pre></p></div>\n".format(
-        slurm_queue
+    response += "<h2>Cluster information</h2>\n"
+    response += "<div style=\"background-color: white; color: black; \"><p>\n<pre>{}</pre></p></div>\n".format(
+        "sinfo"
     )
-    response += "<h2>sinfo</h2>\n"
     response += "<div style=\"background-color: black; color: white; \"><p>\n<pre>{}</pre></p></div>\n".format(
         slurm_info
     )
+    response = "<h2>Current queue</h2>\n"
+    response += "<div style=\"background-color: white; color: black; \"><p>\n<pre>{}</pre></p></div>\n".format(
+        "squeue"
+    )
+    response += "<div style=\"background-color: black; color: white; \"><p>\n<pre>{}</pre></p></div>\n".format(
+        slurm_queue
+    )
+    response += "<h2>Configuration</h2>\n"
+    response += "<div> style="\"background-color: white; color: black; \"><p>\n<pre>{}</pre></p></div>\n".format(
+        "cat /etc/slurm-llnl/slurm.conf"
+    }
+    response += "<div> style="\"background-color: black; color: white; \"><p>\n<pre>{}</pre></p></div>\n".format(
+        slurm_config
+    }
     response += "<p>\nVersion: {}</p>\n".format(slurm_version)
     response += "<p>\nDate: {}</p>\n".format(slurm_date)
 
